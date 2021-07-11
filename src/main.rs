0c6365f6201d572522fb238a12fdd1dfb3fbc429
cosmobobak <56003038+cosmobobak@users.noreply.github.com>
2021-07-11 13:54:05 +0100

    new files

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+mod sudoku;
+
+use sudoku::SudokuBoard;
+
+// const easy: &str = "----345----89---3-3----27892-4--6815----4----8765--4-27523----6-1---79----942----";
+// const medium: &str = "---4-6-9------3--545-----866-2-74--1----9----9--56-7-871-----643--6------6-9-2---";
+// const hard: &str = "9-3--42--4-65-------28----------5--4-67-4-92-1--9----------87-------94-3--83--6-1";
+// const evil: &str = "--9------384---5------4-3-----1--27-2--3-4--5-48--6-----6-1------7---629-----5---";
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() <= 1 {
+        println!("no input string provided.");
+        std::process::exit(0);
+    }
+
+    if !SudokuBoard::is_string_valid(&args[1]) {
+        println!("input string invalid (you may only use digits and dashes in your input).");
+        println!("The problematic string: \"{}\"", args[1]);
+        std::process::exit(0);
+    }
+    // object is created
+    let mut b = SudokuBoard::from_string(&args[1]);
+
+    // show the user their initial board, to confirm to
+    // them that they have entered the correct CLI string
+    println!("\nYour sudoku:");
+    b.show();
+
+    // check if the given sudoku is legal as-is, exit early if not
+    if b.current_state_invalid() {
+        println!("input sudoku invalid (given problem has repeated digits in rows, columns, or squares).");
+        std::process::exit(0);
+    }
+
+    let start = std::time::Instant::now();
+
+    let success = b.solve();
+
+    // if the solve was unsuccessful, then the given sudoku was bad, and we exit early
+    if !success {
+        println!("overconstrained sudoku (there is no pattern of digits that can validly fill the given sudoku).");
+        std::process::exit(0);
+    }
+
+    let time_taken = start.elapsed();
+    
+    // show the solved sudoku and exit
+    println!("Your solved sudoku:");
+    b.show();
+    println!("\nsolved in {:.2?}!", time_taken);
+    std::process::exit(0);
+}
---
+++ src/sudoku.rs
@@ -0,0 +1,109 @@
+use array2d::Array2D;
+
+const UNASSIGNED: u8 = 0;
+const SYMBOLS: [char; 10] = ['.', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const VALID_TOKENS: [char; 10] = ['-', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+#[derive(Clone)]
+pub struct SudokuBoard {
+    state: Array2D<u8>
+}
+
+pub fn square(state: &Array2D<u8>, x: usize) -> [u8; 9] {
+    let rstart = ((x / 9) / 3) * 3;
+    let cstart = ((x % 9) / 3) * 3;
+    let mut out = [0u8; 9];
+    for row in rstart..(3+rstart) {
+        for col in cstart..(3+cstart) {
+            out[(row - rstart) * 3 + (col - cstart)] = state[(row, col)];
+        }
+    }
+    return out;
+}
+
+impl Default for SudokuBoard {
+    fn default() -> SudokuBoard {
+        return SudokuBoard { state: Array2D::filled_with(0u8, 9, 9) }
+    }  
+}
+
+impl SudokuBoard {
+    pub fn from_string(fen: &String) -> SudokuBoard {
+        let mut out = SudokuBoard::default();
+        out.set_from_string(fen);
+        return out;
+    }
+
+    fn set_from_string(&mut self, fen: &String) {
+        for (i, c) in fen.chars().enumerate() {
+            if c != '-' {
+                self.state[(i / 9, i % 9)] = c.to_digit(10).expect("this should have been validated >:(") as u8;
+            }
+        }
+    }
+
+    pub fn is_string_valid(fen: &String) -> bool {
+        return fen.chars().all(|c| VALID_TOKENS.iter().any(|token| *token == c));
+    }
+
+    pub fn show(&self) {
+        for row in self.state.rows_iter() {
+            for num in row {
+                print!("{} ", SYMBOLS[*num as usize]);
+            }
+            println!();
+        }
+        println!();
+    }
+
+    pub fn first_unassigned(&self) -> usize {
+        for i in 0..81 {
+            if self.state[(i / 9, i % 9)] == 0u8 {
+                return i;
+            }
+        }
+        return 81;
+    }
+
+    pub fn current_state_invalid(&mut self) -> bool {
+        let mut n;
+        for i in 0..81 {
+            n = self.state[(i / 9, i % 9)];
+            if n != 0 {
+                self.state[(i / 9, i % 9)] = 0;
+                if !self.legal(i, n) {
+                    return true;
+                }
+                self.state[(i / 9, i % 9)] = n;
+            }
+        }
+        return false;
+    }
+
+    fn legal(&self, x: usize, num: u8) -> bool {
+        return self.state.row_iter(x / 9 as usize)
+                         .all(|n| *n != num) &&
+               self.state.column_iter(x % 9 as usize)
+                         .all(|n| *n != num) &&
+               square(&self.state, x as usize).iter()
+                         .all(|n| *n != num);
+    }
+
+    pub fn solve(&mut self) -> bool {
+        let x = self.first_unassigned();
+        // If there is no unassigned location, we are done
+        if x == 81 {
+            return true;  // success!
+        }
+        for num in 1..10 {
+            if self.legal(x, num) {
+                self.state[(x / 9, x % 9)] = num;
+                if self.solve() {
+                    return true;
+                }
+                self.state[(x / 9, x % 9)] = UNASSIGNED;
+            }
+        }
+        return false;  // this triggers backtracking
+    }
+}

a2dd8f51a168bcd28fa7d5ec38e00f55540f770b
cosmobobak <cosmobobak@gmail.com>
2021-10-22 14:22:09 +0100

    critical bugfix, perf improvement

--- src/sudoku.rs
+++ src/sudoku.rs
@@ -1,6 +1,10 @@
 const UNASSIGNED: u8 = 0;
 const SYMBOLS: [char; 10] = ['.', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const VALID_TOKENS: [char; 10] = ['-', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const DIVIDER: &str = "├───────┼───────┼───────┤\n";
+const TOP: &str = "┌───────┬───────┬───────┐\n";
+const BOTTOM: &str = "└───────┴───────┴───────┘\n";
+const BAR: &str = "│ ";
 
 #[derive(Clone)]
 pub struct SudokuBoard {
@@ -8,6 +12,13 @@ pub struct SudokuBoard {
 }
 
 struct BoxIterator<'a> {
+    row: usize,
+    col: usize,
+    sentinel: usize,
+    board: &'a SudokuBoard
+}
+
+struct GlobalIterator<'a> {
     row: usize,
     col: usize,
     sentinel: usize,
@@ -28,8 +39,35 @@ impl BoxIterator<'_> {
     }
 
     fn next(&mut self) -> Option<u8> {
+        let result = if self.row == self.sentinel {
+            None 
+        } else {
+            Some(self.board.state[self.row][self.col])
+        };
         if (self.col) % 3 == 2 {
             self.col -= 2;
+            self.row += 1;
+        } else {
+            self.col += 1;
+        }
+        result
+    }
+}
+
+#[allow(dead_code)]
+impl GlobalIterator<'_> {
+    fn new(board: &SudokuBoard) -> GlobalIterator {
+        GlobalIterator {
+            row: 0,
+            col: 0,
+            sentinel: 9,
+            board
+        }
+    }
+
+    fn next(&mut self) -> Option<u8> {
+        if self.col == 8 {
+            self.col = 0;
             self.row += 1;
         } else {
             self.col += 1;
@@ -50,18 +88,12 @@ impl Iterator for BoxIterator<'_> {
     }
 }
 
-impl Default for SudokuBoard {
-    fn default() -> SudokuBoard {
-        SudokuBoard { state: [[0; 9]; 9] }
-    }  
-}
-
 impl SudokuBoard {
     pub fn from_string(fen: &str) -> Result<SudokuBoard, String> {
         if !SudokuBoard::is_string_valid(fen) {
             return Err(format!("input string invalid (you may only use digits and dashes in your input): \"{}\"", fen));
         }
-        let mut out = SudokuBoard::default();
+        let mut out = SudokuBoard { state: [[0; 9]; 9] };
         out.set_from_string(fen);
         match out.current_state_invalid() {
             false => Ok(out),
@@ -82,13 +114,18 @@ impl SudokuBoard {
     }
 
     pub fn show(&self) {
-        for row in self.state {
-            for num in row {
-                print!("{} ", SYMBOLS[num as usize]);
-            }
-            println!();
-        }
         println!();
+        print!("{}", TOP);
+        for y in 0..9 {
+            print!("{}", BAR);
+            for x in 0..9 {
+                print!("{} ", SYMBOLS[ self.state[y][x] as usize ]);
+                if x % 3 == 2 && x != 8 { print!("{}", BAR); }
+            }
+            println!("{}", BAR);
+            if y % 3 == 2 && y != 8 { print!("{}", DIVIDER); };
+        }
+        print!("{}", BOTTOM);
     }
 
     pub fn first_unassigned(&self) -> usize {

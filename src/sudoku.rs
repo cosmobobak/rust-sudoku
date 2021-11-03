b6134b58f3df145f01b237cd06f74ce1bb6985ee
cosmobobak <cosmobobak@gmail.com>
2021-11-03 17:46:08 +0000

    bugfixes and better style

--- src/sudoku.rs
+++ src/sudoku.rs
@@ -1,6 +1,5 @@
 const UNASSIGNED: u8 = 0;
 const SYMBOLS: [char; 10] = ['.', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-const VALID_TOKENS: [char; 10] = ['-', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const DIVIDER: &str = "├───────┼───────┼───────┤\n";
 const TOP: &str = "┌───────┬───────┬───────┐\n";
 const BOTTOM: &str = "└───────┴───────┴───────┘\n";
@@ -9,21 +8,20 @@ const BAR: &str = "│ ";
 #[derive(Clone)]
 pub struct SudokuBoard {
     state: [[u8; 9]; 9],
-    first_unassigned: usize
+    first_unassigned: usize,
 }
 
 struct BoxIterator<'a> {
     row: usize,
     col: usize,
     sentinel: usize,
-    board: &'a SudokuBoard
+    board: &'a SudokuBoard,
 }
 
-struct GlobalIterator<'a> {
+pub struct GlobalIterator<'a> {
     row: usize,
     col: usize,
-    sentinel: usize,
-    board: &'a SudokuBoard
+    board: &'a SudokuBoard,
 }
 
 impl BoxIterator<'_> {
@@ -35,13 +33,13 @@ impl BoxIterator<'_> {
             row,
             col,
             sentinel,
-            board
+            board,
         }
     }
 
     fn next(&mut self) -> Option<u8> {
         let result = if self.row == self.sentinel {
-            None 
+            None
         } else {
             Some(self.board.state[self.row][self.col])
         };
@@ -55,29 +53,28 @@ impl BoxIterator<'_> {
     }
 }
 
-#[allow(dead_code)]
 impl GlobalIterator<'_> {
     fn new(board: &SudokuBoard) -> GlobalIterator {
         GlobalIterator {
             row: 0,
             col: 0,
-            sentinel: 9,
-            board
+            board,
         }
     }
 
     fn next(&mut self) -> Option<u8> {
+        let result = if self.row == 9 {
+            None
+        } else {
+            Some(self.board.state[self.row][self.col])
+        };
         if self.col == 8 {
             self.col = 0;
             self.row += 1;
         } else {
             self.col += 1;
         }
-        if self.row == self.sentinel {
-            None 
-        } else {
-            Some(self.board.state[self.row][self.col])
-        }
+        result
     }
 }
 
@@ -89,42 +86,63 @@ impl Iterator for BoxIterator<'_> {
     }
 }
 
+impl Iterator for GlobalIterator<'_> {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        GlobalIterator::next(self)
+    }
+}
+
 impl SudokuBoard {
     pub fn from_string(fen: &str) -> Result<SudokuBoard, String> {
         if !SudokuBoard::is_string_valid(fen) {
-            return Err(format!("input string invalid (you may only use digits and dashes in your input): \"{}\"", fen));
-        }
-        let mut out = SudokuBoard { state: [[0; 9]; 9], first_unassigned: 0 };
-        out.set_from_string(fen);
-        match out.current_state_invalid() {
-            false => Ok(out),
-            true => Err("input sudoku invalid (given problem has repeated digits in rows, columns, or squares).".to_string())
+            Err(format!(
+                "input string invalid (you may only use digits and dashes in your input): \"{}\"",
+                fen
+            ))
+        } else {
+            let mut out = SudokuBoard {
+                state: [[0; 9]; 9],
+                first_unassigned: 0,
+            };
+            out.set_from_string(fen);
+            match out.current_state_invalid() {
+                false => Ok(out),
+                true => Err("input sudoku invalid (given problem has repeated digits in rows, columns, or squares).".to_string())
+            }
         }
     }
 
     fn set_from_string(&mut self, fen: &str) {
         for (i, c) in fen.chars().enumerate() {
             if c != '-' {
-                self.state[i / 9][i % 9] = c.to_digit(10).expect("this should have been validated >:(") as u8;
+                self.state[i / 9][i % 9] =
+                    c.to_digit(10).expect("this should have been validated >:(") as u8;
             }
         }
     }
 
     fn is_string_valid(fen: &str) -> bool {
-        return fen.chars().all(|c| VALID_TOKENS.iter().any(|token| *token == c));
+        fen.chars()
+            .all(|c| c == '-' || c.is_ascii_digit() && c != '0')
     }
 
     pub fn show(&self) {
         println!();
         print!("{}", TOP);
-        for y in 0..9 {
+        for (i, row) in self.state.iter().enumerate() {
             print!("{}", BAR);
-            for x in 0..9 {
-                print!("{} ", SYMBOLS[ self.state[y][x] as usize ]);
-                if x % 3 == 2 && x != 8 { print!("{}", BAR); }
+            for (j, val) in row.iter().enumerate() {
+                print!("{} ", SYMBOLS[*val as usize]);
+                if j % 3 == 2 && j != 8 {
+                    print!("{}", BAR);
+                }
             }
             println!("{}", BAR);
-            if y % 3 == 2 && y != 8 { print!("{}", DIVIDER); };
+            if i % 3 == 2 && i != 8 {
+                print!("{}", DIVIDER);
+            };
         }
         print!("{}", BOTTOM);
     }
@@ -157,24 +175,25 @@ impl SudokuBoard {
         false
     }
 
+    pub fn iter(&self) -> GlobalIterator {
+        GlobalIterator::new(self)
+    }
+
     fn box_iter(&self, x: usize) -> BoxIterator {
         BoxIterator::new(x, self)
     }
 
     fn legal(&self, x: usize, num: u8) -> bool {
-        return self.state[x / 9].iter()
-                         .all(|n| *n != num) &&
-               self.state.iter()
-                         .all(|row| row[x % 9] != num) &&
-               self.box_iter(x)
-                         .all(|n| n != num);
+        self.state[x / 9].iter().all(|n| *n != num)
+            && self.state.iter().all(|row| row[x % 9] != num)
+            && self.box_iter(x).all(|n| n != num)
     }
 
     pub fn solve(&mut self) -> bool {
         let x = self.first_unassigned();
         // If there is no unassigned location, we are done
         if x == 81 {
-            return true;  // success!
+            return true; // success!
         }
         for num in 1..10 {
             if self.legal(x, num) {
@@ -186,6 +205,6 @@ impl SudokuBoard {
                 self.first_unassigned = x;
             }
         }
-        false  // this triggers backtracking
+        false // this triggers backtracking
     }
 }
